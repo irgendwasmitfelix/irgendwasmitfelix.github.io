f1b8e8b5e60ac13c1e17fa7ce51f288e3addfaa3
Felix H <florian.helleckes@gmail.com>
2023-10-18 21:15:19 +0200

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -121,7 +121,7 @@ const config = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'wireframe',
+    defaultTheme: 'black',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
