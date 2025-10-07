527d7e16d9076dfe572067cad392582b68e17ca6
irgendwasmitfelix <florian.helleckes@gmail.com>
2025-10-07 22:43:46 +0200

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -30,7 +30,7 @@ const config = {
   },
   resume: {
     fileUrl:
-      'https://drive.google.com/file/d/1lgacTN0f1dQsYgJG96Lg2RB_BlD5dllz/view?usp=sharing', // Empty fileUrl will hide the `Download Resume` button.
+      'https://drive.google.com/file/d/1DHcOn4y6RCx03eOqG7vOEOMCtoijqdvr/view?usp=sharing', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'Playwright',
