cbbe0ed3294673d3e342a893b3eda8493b67e527
irgendwasmitfelix <florian.helleckes@gmail.com>
2025-12-21 22:09:30 +0100

    fix hope

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -7,7 +7,7 @@ const config = {
     limit: 10, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
-      projects: ['assessment','irgendwasmitfelix.github.io'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+      projects: [,'irgendwasmitfelix.github.io'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
   social: {
