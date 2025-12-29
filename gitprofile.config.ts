8c52e7b1f0fc085c781cefc5d7fda69dec2e4fc1
irgendwasmitfelix <florian.helleckes@gmail.com>
2025-12-29 17:11:39 +0100

    Fix config structure and add external projects

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -10,6 +10,60 @@ const config = {
       projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
+  projects: {
+    github: {
+      display: true,
+      header: 'My GitHub Projects',
+      mode: 'automatic',
+    },
+    external: {
+      header: 'Other Projects',
+      projects: [
+        {
+          title: 'And many more in app, IoT and web development',
+          description:
+            'I am not able to mention by name here, unfortunately. Please feel free to ask in Person :)',
+          imageUrl: '',
+          link: 'mailto:f.helleckes@proton.me',
+        },
+        {
+          title: 'Peek & Cloppenburg Web Apps',
+          description:
+            'Peek & Cloppenburg is a German fashion retailer founded in 1869. With a bunch of employees there was a lot of internal projects which we developed and I also got in touch with E Commerce Development and automated a lot of stuff with webdriver and locust.io',
+          imageUrl: 'https://play-lh.googleusercontent.com/yycoMy1dTRJMFbwyvIpctd44_T7MSvZF-oQ9jnrPDdzqJad2Lirrvjep3aLpx4D8_pQ',
+          link: 'https://www.peek-cloppenburg.de/',
+        },
+        {
+          title: 'KFW',
+          description:
+            'KfW, or Kreditanstalt für Wiederaufbau, is a German development bank. It is owned by the German government and provides loans and other financial services to businesses, governments, and individuals. KfW`s main focus is on promoting economic development and social welfare in Germany',
+          imageUrl: 'https://www.kfw-ipex-bank.de/Techn-Medien/Logos/Social-Media/KfW_IPEX_Logo_180x180px.png',
+          link: 'https://www.kfw.de/kfw.de.html',
+        },
+        {
+          title: 'Bega - The Good Light',
+          description:
+            'The bega connect gateway is a smart home gateway that supports Zigbee and DALI. It allows you to connect and control Zigbee and DALI devices with your smartphone or tablet.',
+          imageUrl: 'https://image.winudf.com/v2/image1/ZGUuYmVnYS5iZWdhY29ubmVjdF9pY29uXzE2MTc5NjQwMTFfMDky/icon.png?w=184&fakeurl=1',
+          link: 'https://connect.bega.com/en',
+        },
+        {
+          title: 'Migros App',
+          description:
+            'Migros Genossenschaft is the largest Swiss retailer and one of the 40 largest retailers worldwide. Within the group, almost all relevant retail services are offered. With its restaurants, for example, Migros is one of the largest gastronomy companies in Switzerland. In addition to grocery stores, it also has DIY stores, electronics stores, sporting goods stores and furniture stores under its roof.',
+          imageUrl: 'https://corporate.migros.ch/_next/static/media/mLogo.abe368b1.svg',
+          link: 'https://grandcentrix.net/de/references/migros/',
+        },
+        {
+          title: 'Leica Camera App',
+          description:
+            'The first almost 2 years at Grandcentrix I worked on the Leica camera app. I created a test concept for various cameras and a wide portfolio of Android and iOS versions. In addition, I managed the entire test process in Scrum, partly also with external service providers (personnel responsibility).',
+          imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Leica_Camera_logo.svg/2048px-Leica_Camera_logo.svg.png',
+          link: 'https://grandcentrix.net/de/references/leica',
+        },
+      ],
+    },
+  },
   seo: {
     title: 'Felix Helleckes - QA Engineer & Portfolio',
     description: 'Portfolio von Felix Helleckes, QA-Engineer mit Expertise in Test Automation, Playwright, Webdriver.io und CI/CD',
@@ -101,51 +155,6 @@ const config = {
     // },
   ],
 
-  // To hide the `My Projects` section, keep it empty.
-  externalProjects: [
-    {
-      title: 'And many more in app, IoT and web development',
-      description:
-      'I am not able to mention by name here, unfortunately. Please feel free to ask in Person :)',
-      imageUrl: '',
-      link: 'mailto:f.helleckes@proton.me',
-    },
-    {
-      title: 'Peek & Cloppenburg Web Apps',
-      description:
-      'Peek & Cloppenburg is a German fashion retailer founded in 1869. With a bunch of employees there was a lot of internal projects which we developed and I also got in touch with E Commerce Development and automated a lot of stuff with webdriver and locust.io',
-      imageUrl: 'https://play-lh.googleusercontent.com/yycoMy1dTRJMFbwyvIpctd44_T7MSvZF-oQ9jnrPDdzqJad2Lirrvjep3aLpx4D8_pQ',
-      link: 'https://www.peek-cloppenburg.de/',
-    },
-    {
-      title: 'KFW',
-      description:
-      'KfW, or Kreditanstalt für Wiederaufbau, is a German development bank. It is owned by the German government and provides loans and other financial services to businesses, governments, and individuals. KfW`s main focus is on promoting economic development and social welfare in Germany',
-      imageUrl: 'https://www.kfw-ipex-bank.de/Techn-Medien/Logos/Social-Media/KfW_IPEX_Logo_180x180px.png',
-      link: 'https://www.kfw.de/kfw.de.html',
-    },
-    {
-      title: 'Bega - The Good Light',
-      description:
-        'The bega connect gateway is a smart home gateway that supports Zigbee and DALI. It allows you to connect and control Zigbee and DALI devices with your smartphone or tablet.',
-      imageUrl: 'https://image.winudf.com/v2/image1/ZGUuYmVnYS5iZWdhY29ubmVjdF9pY29uXzE2MTc5NjQwMTFfMDky/icon.png?w=184&fakeurl=1',
-      link: 'https://connect.bega.com/en',
-    },
-    {
-      title: 'Migros App',
-      description:
-        'Migros Genossenschaft is the largest Swiss retailer and one of the 40 largest retailers worldwide. Within the group, almost all relevant retail services are offered. With its restaurants, for example, Migros is one of the largest gastronomy companies in Switzerland. In addition to grocery stores, it also has DIY stores, electronics stores, sporting goods stores and furniture stores under its roof.',
-      imageUrl: 'https://corporate.migros.ch/_next/static/media/mLogo.abe368b1.svg',
-      link: 'https://grandcentrix.net/de/references/migros/',
-    },
-    {
-      title: 'Leica Camera App',
-      description:
-        'The first almost 2 years at Grandcentrix I worked on the Leica camera app. I created a test concept for various cameras and a wide portfolio of Android and iOS versions. In addition, I managed the entire test process in Scrum, partly also with external service providers (personnel responsibility).',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Leica_Camera_logo.svg/2048px-Leica_Camera_logo.svg.png',
-      link: 'https://grandcentrix.net/de/references/leica',
-    },
-  ],
   // Display blog posts from your medium or dev account. (Optional)
   blog: {
     source: 'medium', // medium | dev
