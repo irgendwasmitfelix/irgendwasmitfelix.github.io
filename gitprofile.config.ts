f439543739f074cc943c2baeb3b096cf69cd135e
irgendwasmitfelix <florian.helleckes@gmail.com>
2025-12-29 17:26:52 +0100

    deactivate ganalytics

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -162,7 +162,7 @@ const config = {
     limit: 2, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
-    id: 'G-1EKFNM3HR5', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   },
   // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: {
